4da276953527637c40c2a30511b5f4196f2faf2c
Assia Deramchi <at.deramchi@gmail.com>
2020-07-06 12:51:50 +0100

    added target blank to a tags

--- src/components/projects/Proj2.js
+++ src/components/projects/Proj2.js
@@ -12,7 +12,7 @@ const Proj2 = () => {
           <source src={clip2} type="video/mp4"/>
         </video>
       </div>
-      <a href="https://nasa-archives.netlify.app/">
+      <a href="https://nasa-archives.netlify.app/" target="_blank" rel="noreferrer">
         <button className='goto-buttons'>Go To Live Site</button>
       </a><br></br>
       <p className="about-proj">
@@ -22,7 +22,7 @@ const Proj2 = () => {
         Finding such an interesting API with many ways to GET different collections of images/information was both a win and also a challenge. There was lots of information work with but it also meant the requests could be a little more complicated than other API's. Most notably the project search and display. As the inital request for the projects only returned their date and ID, we had to take the ID upon selecting and fire off a new GET request using that ID. The user-end process is seemless and I am pleased with the outcome.
  
       </p>
-      <a href="https://github.com/A-Der/sei-project-2">
+      <a href="https://github.com/A-Der/sei-project-2" target="_blank" rel="noreferrer">
         <button className='goto-buttons'>See Full Read Me</button>
       </a><br></br>
       <div style={{ height: '1em', width: '75%' }}>
